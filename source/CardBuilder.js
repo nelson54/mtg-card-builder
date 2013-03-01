3b30e6a75b76e55cbc0a3c63686b49672483b03d
Derek Nelson <dereknelson01@gmail.com>
2013-02-28 21:29:35 -0500

    CardBuilder

--- source/CardBuilder.js
+++ source/CardBuilder.js
@@ -2,7 +2,7 @@
  * Created by IntelliJ IDEA.
  * User: dnelson
  * Date: 2/28/13
- * Time: 5:29 PM
+ * Time: 7:29 PM
  * To change this template use File | Settings | File Templates.
  */
 
--- specs/CardBuilderSpec.js
+++ specs/CardBuilderSpec.js
@@ -15,15 +15,15 @@ describe("A CardBuilder", function(){
 
     it("can make a new CardBuilder", function(){
       expect(typeof new CardBuilder()).toBe('object');
-    })
+    });
 
     it("can make named card", function(){
       expect(runeclawBear.name).toBe(name);
-    })
+    });
 
     it("can make a creature with power and toughness", function(){
         expect(runeclawBear.types.indexOf("creature")).not.toEqual(-1);
         expect(runeclawBear.power).toBe(2);
         expect(runeclawBear.toughness).toBe(2);
-    })
+    });
 });
