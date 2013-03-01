278b278e7d5f9ef196ce6fca1a27f86b893c2824
Derek Nelson <dereknelson01@gmail.com>
2013-02-28 19:43:15 -0500

    Initialized

---
+++ src-test/CardBuilderSpec.js
@@ -0,0 +1,29 @@
+describe("A CardBuilder", function(){
+
+    var name = "Runeclaw Bear",
+        type = "Bear",
+        expansion = "M12",
+        powerToughness = 2;
+
+    var runeclawBear = new CardBuilder()
+        .Name(name)
+        .Expansion(expansion)
+        .Creature()
+        .CreatureType(type)
+        .Power(powerToughness)
+        .Toughness(powerToughness);
+
+    it("can make a new CardBuilder", function(){
+      expect(typeof new CardBuilder()).toBe('object');
+    })
+
+    it("can make named card", function(){
+      expect(runeclawBear.name).toBe(name);
+    })
+
+    it("can make a creature with power and toughness", function(){
+        expect(runeclawBear.types.indexOf("creature")).not.toBe(-1);
+        expect(runeclawBear.power).toBe(2);
+        expect(runeclawBear.toughness).toBe(2);
+    })
+});
---
+++ src/CardBuilder.js
@@ -0,0 +1,138 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: dnelson
+ * Date: 2/28/13
+ * Time: 5:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var CardBuilder = function(){
+    this.name = "UNKNOWN";
+    this.types = [];
+
+    this.isLegendary = false;
+    this.hasFlash = false;
+
+    this.FlavorText = function(text){
+        this.flavorText = text;
+    };
+
+    this.Name = function(name){
+        this.name = name;
+        return this;
+    };
+
+    this.Expansion = function(name){
+        this.expansion = name;
+        return this;
+    };
+
+    this.Set = this.Expansion;
+
+    this.Flash = function(){
+        this.hasFlash = true;
+    };
+
+
+    //  Types
+    this.Enchantment = function(){
+        this.types.push("enchantment");
+        return this;
+    };
+
+    this.Instant = function(){
+        this.types.push("instant");
+        return this;
+    };
+
+    this.Tribal = function(){
+        this.types.push("instant");
+        return this;
+    };
+
+    this.Sorcery = function(){
+        this.types.push("sorcery");
+        return this;
+    };
+
+    this.Creature = function(){
+        this.types.push("creature");
+
+        this.creatureTypes = [];
+
+        this.hasSummoningSickness = true;
+
+        return this;
+    };
+
+    this.Artifact = function(){
+        this.types.push("artifact");
+        return this;
+    };
+
+    this.Land = function(){
+        this.types.push("land");
+        this.landTypes = [];
+        return this;
+    };
+
+    this.Planeswalker = function(){
+        this.planeswalkerAbilities = [];
+        this.PlaneswalkerType("UNDEFINED");
+        this.Loyalty(0);
+        this.types.push("planeswalker");
+        return this;
+    };
+
+    //Planeswalkers
+    this.PlaneswalkerType = function(type){
+        if(typeof this.planeswalkerType != "string")
+            throw "This must be a planeswalker.";
+        this.planeswalkerType.push(type);
+
+        return this;
+    };
+
+    this.PlaneswalkerAbility = function(loyaltyDelta, ability, text){
+        if(typeof this.planeswalkerAbilities != "array")
+            throw "This must be a planeswalker.";
+        this.planeswalkerAbilities.push({Delta: loyaltyDelta, Ability: ability, Text: text});
+        return this;
+    };
+
+    this.Loyalty = function(number){
+        this.loyalty = number;
+        return this;
+    };
+
+    //  Lands
+    this.LandType = function(type){
+        if(typeof this.landTypes != "array")
+            throw "This must be a land.";
+        this.landTypes.push(type);
+        return this;
+    };
+
+    //  Creatures
+    this.CreatureType = function(type){
+        if(typeof this.creatureTypes != "array")
+            throw "This must be a creature.";
+        this.creatureTypes.push();
+        return this;
+    };
+
+    this.Power = function(power){
+        this.power = power;
+        return this;
+    };
+
+    this.Toughness = function(toughness){
+        this.toughness = toughness;
+        return this;
+    };
+
+    this.Haste = function(){
+        this.hasSummoningSickness = false;
+    };
+};
